4a76b86e5153780b9adf769970b4979dc7c552e0
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2022-04-27 15:41:27 +0530

    useLocation and useHistory hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import {BrowserRouter, Route} from 'react-router-dom'
-import NavBar from './Components/Navbar';
-import AllRoutes from './Routes/AllRoutes';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import NavBar from "./Components/Navbar";
+import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
   return (
-   <BrowserRouter>
-      <NavBar/>
-      <AllRoutes/>
-   </BrowserRouter>
-  )
+    <BrowserRouter>
+      <NavBar />
+      <AllRoutes />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
 const About = () => {
+  const location = useLocation();
+
+  console.log(location.pathname);
+
+  const history = useHistory();
+  console.log(history);
+  const goBack = () => {
+    history.goForward();
+  };
   return (
     <>
       <h3>About Page</h3>
@@ -8,6 +18,7 @@ const About = () => {
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem sunt
         totam obcaecati illum corporis, eaque eveniet blanditiis qui. In minima,
       </p>
+      <button onClick={goBack}> go Forward </button>
     </>
   );
 };
